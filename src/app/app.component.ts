8f3aa61a863613eabc2b261ce530f96fe1e0f124
Uttam Arade <uttam.arade@vyomlabs.com>
2017-03-09 19:24:51 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Chart } from 'chart.js'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    let othis = this;
+    this.init(function() {
+      othis.getCanvas();
+    }.bind(this));
+  }
+
+  init(callback) {
+    callback();
+  }
+
+  getCanvas() {
+    const CANVAS = document.getElementById('myCanvas');
+    console.log(CANVAS);
+
+    let lineChart = new Chart(CANVAS, {
+      type: 'bar',
+      data: {
+        labels: ["Workflow 1", "Workflow 2", "Workflow 3", "Workflow 4", "Workflow 5", "Workflow 6", "Workflow 7",
+          "Workflow 1", "Workflow 2", "Workflow 3", "Workflow 4", "Workflow 5", "Workflow 6", "Workflow 7",
+          "Workflow 1", "Workflow 2", "Workflow 3", "Workflow 4", "Workflow 5", "Workflow 6", "Workflow 7"],
+        datasets: [
+          {
+            label: "Successful requests",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(70, 129, 152, 0.61)",
+            borderColor: "#006699",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "#006699",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "#006699",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 95,65, 59, 80, 81, 56, 55, 95,65, 59, 80, 81, 56, 55, 95],
+            spanGaps: false,
+          },
+          {
+            label: "Failed requests",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(224, 55, 55, 0.51)",
+            borderColor: "red",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "red",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "red",
+            pointHoverBorderColor: "red",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [0, 14, 0, 46, 34, 15, 6,65, 59, 80, 41, 56, 23, 26,65, 34, 80, 81, 11, 55, 21],
+            spanGaps: false,
+          }
+        ]
+      }
+    });
+  }
 }
